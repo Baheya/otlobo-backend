dfbd103450fd86bc11536d15c595afe1fcf3ad3d
Baheya <47673768+Baheya@users.noreply.github.com>
2019-07-09 15:12:58 +0200

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('otlobo', 'beya', 'nantsigomenyabaghitibaba', {
+const sequelize = new Sequelize('otlobo', 'beya', '', {
   dialect: 'postgresql',
   host: 'localhost'
 });
