18414980b86fe0d0e62f233e30d7002dc0377f13
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-07-09 22:35:55 +0200

    added getRestaurant controller

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const Restaurant = require('../models/restaurant');
+const MenuItem = require('../models/menu-item');
 
 exports.getRestaurants = (req, res, next) => {
   Restaurant.find()
@@ -7,6 +8,33 @@ exports.getRestaurants = (req, res, next) => {
       res.status(200).json({
         message: 'Restaurants fetched successfully.',
         posts: restaurants
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getRestaurant = (req, res, next) => {
+  const restaurantId = req.body.restaurantId;
+  Restaurant.findAll({
+    where: {
+      id: 1
+    },
+    include: [
+      {
+        model: MenuItem,
+        as: 'menu_items'
+      }
+    ]
+  })
+    // Restaurant.findByPk(1)
+    //   include: [{ model: MenuItem, where: { restaurantId: 1 } }]
+    // })
+    .then(restaurant => {
+      res.status(200).json({
+        message: 'Restaurant fetched successfully.',
+        restaurant: restaurant
       });
     })
     .catch(err => {
