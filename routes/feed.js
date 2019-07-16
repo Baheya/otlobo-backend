517b81234b33cb030143bfe996e748e795d7dfa3
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-07-16 14:00:56 +0200

    added new route

--- routes/feed.js
+++ routes/feed.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const feedController = require('../controllers/feed');
-const isAuth = require('../middleware/is-auth')
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get('/restaurants', feedController.getRestaurants);
 
 router.get('/restaurant/:restaurantId', feedController.getRestaurant);
+
+router.post('/restaurant/:restaurantId', feedController.addOrder);
 
 module.exports = router;
